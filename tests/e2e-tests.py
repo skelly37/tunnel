bb37969bd8f6545f538440fe36ae3c4e6e333712
skelly37 <skelly37@protonmail.com>
2024-08-13 14:46:54 +0200

    prepared tests cases for e2e

---
+++ tests/e2e-tests.py
@@ -0,0 +1,14 @@
+# basic setup:
+# - start server in some thread
+# - create sender thread that sends some file
+# - create receiver that receives it
+
+
+# test cases:
+# 1) invalid sender args
+# 2) invalid receiver args
+# 3) transfer declined by receiver
+# 4) invalid receiver link
+# 5) accepted transfer of some small file (hello world)
+# 6) accepted transfer of docs directory with preview.mp4
+# 7) accepted transfer of some dummy structure (just like in utils tests)
